895e954b11757c31c9edc37ae06621d225cef8dd
Nghiatruonggg <nghiatruong110700@gmail.com>
2026-02-19 11:52:20 +1100

    test: health + root route check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
-import { AuthModule } from '../modules/auth';
+//import { DatabaseModule } from '../database/database.module';
+//import { AuthModule } from '../modules/auth';
 import { HealthModule } from '../modules/health';
 import { RootModule } from '../modules/root';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, RootModule, HealthModule],
+  // imports: [DatabaseModule, AuthModule, RootModule, HealthModule],
+  imports: [HealthModule, RootModule],
 })
 export class AppModule {}
